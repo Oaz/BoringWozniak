f2a90d20af524d95efbfaeae330492ac36879a59
Olivier Azeau <github@azeau.com>
2016-09-10 15:38:06 +0200

    full logic coverage with tests

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Linq;
 
 namespace BoringWozniak
 {
@@ -7,11 +8,39 @@ namespace BoringWozniak
   public class BoringWozniakTest
   {
     [Test]
-    public void WhenRandomNumberIsZero ()
+    public void GenerateNameWithoutRetry ()
     {
-      Func<int,int> zero = x => 0;
-      Assert.That (NamesGenerator.GetRandomName (0, zero), Is.EqualTo(NamesGenerator.left[0]+"_"+NamesGenerator.right[0]));
-      Assert.That (NamesGenerator.GetRandomName (1, zero), Is.EqualTo(NamesGenerator.left[0]+"_"+NamesGenerator.right[0]+"0"));
+      Assert.That (NamesGenerator.GetRandomName (0, FakeRandomNumbers(0,1)), Is.EqualTo(GeneratedName (0,1)));
+    }
+
+    [Test]
+    public void GenerateNameWithRetry ()
+    {
+      Assert.That (NamesGenerator.GetRandomName (1, FakeRandomNumbers(0,1,2)), Is.EqualTo(GeneratedName (0, 1)+"2"));
+    }
+
+    [Test]
+    public void SteveWozniakIsNotBoring ()
+    {
+      var boringIndex = NamesGenerator.left.TakeWhile(val => val != "boring").Count();
+      Assert.That (boringIndex, Is.Not.EqualTo (0));
+      var wozniakIndex = NamesGenerator.right.TakeWhile(val => val != "wozniak").Count();
+      Assert.That (wozniakIndex, Is.Not.EqualTo (0));
+      Assert.That (
+        NamesGenerator.GetRandomName (0, FakeRandomNumbers(boringIndex, wozniakIndex, 0, 0)),
+        Is.EqualTo (GeneratedName (0, 0))
+      );
+    }
+
+    private string GeneratedName(int adjectiveIndex, int surnameIndex)
+    {
+      return NamesGenerator.left [adjectiveIndex] + "_" + NamesGenerator.right [surnameIndex];
+    }
+
+    private Func<int,int> FakeRandomNumbers(params int[] numbers)
+    {
+      var randomCalls = 0;
+      return max => numbers [randomCalls++];
     }
   }
 }
