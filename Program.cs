32ee7f12c20da0a21eb54ff2556d322cf605ff57
Olivier Azeau <github@azeau.com>
2016-09-10 14:57:19 +0200

    initial version converted from golang

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BoringWozniak
+{
+  class MainClass
+  {
+    public static void Main (string[] args)
+    {
+      Console.WriteLine (NamesGenerator.GetRandomName(0));
+      Console.WriteLine (NamesGenerator.GetRandomName(1));
+    }
+  }
+}
